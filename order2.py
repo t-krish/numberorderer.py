ee89b3b31771f2892f93ae3f0856e5c22e04fef3
t-krish <95111595+t-krish@users.noreply.github.com>
2021-11-26 21:03:46 -0500

    Add files via upload

---
+++ order2.py
@@ -0,0 +1,15 @@
+ar = []
+while True:
+    a = input('Numbers: ')
+    try:
+         a = float(a)
+    except:
+        x = 0
+    if a == 'done':
+        break
+    ar.append(a)
+
+ar.sort()
+print(ar)
+#Completed
+    
